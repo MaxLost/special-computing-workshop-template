9e0222503ab3203e8f3468ecfa49da5102f57a60
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-09-20 18:18:56 +0300

    Added solution for Task1

---
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task1/ChangeMachine.java
@@ -0,0 +1,110 @@
+package ru.spbu.apcyb.svp.tasks.task1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ChangeMachine {
+
+  private final int sum;
+  private final int[] coins;
+  private final Logger logger;
+
+  public ChangeMachine() {
+
+    logger = Logger.getLogger(String.valueOf(ChangeMachine.class));
+    logger.setLevel(Level.INFO);
+
+    try (Scanner input = new Scanner(System.in)) {
+
+      System.out.println("Введите сумму для размена:");
+      this.sum = input.nextInt();
+      input.nextLine();
+
+      System.out.println("Введите номиналы для размена:");
+      String[] coinsAsStrings = input.nextLine().split(" ");
+      this.coins = new int[coinsAsStrings.length];
+
+      for (int i = 0; i < coinsAsStrings.length; i++) {
+        this.coins[i] = Integer.parseInt(coinsAsStrings[i]);
+      }
+    }
+
+  }
+
+  public ChangeMachine(int sum, int[] coins) {
+
+    logger = Logger.getLogger(String.valueOf(ChangeMachine.class));
+    logger.setLevel(Level.INFO);
+
+    this.sum = sum;
+    this.coins = coins;
+  }
+
+  public int countChangeOptions() {
+
+    List<int[]> changeOptions = getChangeOptions(sum);
+    HashSet<List<Integer>> uniqueOptions = new HashSet<>();
+
+    for (int[] option : changeOptions) {
+      List<Integer> optionAsList = Arrays.stream(option).boxed().toList();
+      uniqueOptions.add(optionAsList);
+    }
+
+    List<String> optionsAsStrings = convertOptionsToString(uniqueOptions);
+    System.out.println("Количество комбинаций: " + optionsAsStrings.size());
+    for (String option : optionsAsStrings) {
+      System.out.println(option);
+    }
+
+    return optionsAsStrings.size();
+  }
+
+  private List<String> convertOptionsToString(Set<List<Integer>> options) {
+
+    List<String> result = new ArrayList<>();
+
+    for (List<Integer> option : options) {
+      StringBuilder optionAsString = new StringBuilder();
+      for (int i = 0; i < option.size(); i++) {
+        for (int j = 0; j < option.get(i); j++) {
+          optionAsString.append(coins[i]).append(", ");
+        }
+      }
+      int lastCommaIndex = optionAsString.lastIndexOf(",");
+      result.add(optionAsString.substring(0, lastCommaIndex));
+    }
+
+    return result;
+  }
+
+  private List<int[]> getChangeOptions(int s) {
+
+    List<int[]> combinations = new ArrayList<>();
+
+    if (s > 0) {
+
+      for (int i = 0; i < coins.length; i++) {
+
+        if (s == coins[i]) {
+          int[] newCombination = new int[coins.length];
+          newCombination[i] += 1;
+          combinations.add(newCombination);
+        } else {
+          int j = i;
+          List<int[]> previousCombinations = getChangeOptions(s - coins[i]);
+          previousCombinations.forEach(combination -> combination[j] += 1);
+          combinations.addAll(previousCombinations);
+        }
+      }
+    }
+
+    return combinations;
+  }
+
+}
---
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task1/Task1.java
@@ -0,0 +1,16 @@
+package ru.spbu.apcyb.svp.tasks.task1;
+
+/**
+ * Задание 1.
+ */
+public class Task1 {
+
+  public static void main(String[] args) {
+
+    ChangeMachine machine = new ChangeMachine();
+    machine.countChangeOptions();
+  }
+
+
+
+}
