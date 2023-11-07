01db51df9d1712815626e6ddaf607e14e0c1fcb4
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-11-08 00:04:20 +0300

    Optimization

--- src/main/java/ru/spbu/apcyb/svp/tasks/task4/MultithreadComputer.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task4/MultithreadComputer.java
@@ -1,10 +1,14 @@
 package ru.spbu.apcyb.svp.tasks.task4;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,33 +17,35 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Stream;
 
 public class MultithreadComputer {
 
   public static void main(String[] args) {
 
-    if (args.length != 2) {
+    if (args.length != 3) {
       throw new IllegalArgumentException(
           "Incorrect amount of arguments, check specification of program.");
     }
 
     try {
       Path inputPath = Path.of(args[0]);
-      Path outputPath = Path.of(args[1]);
+      Path singleOutputPath = Path.of(args[1]);
+      Path multiOutputPath = Path.of(args[2]);
 
-      computeTangents(inputPath, outputPath);
+      computeTangents(inputPath, singleOutputPath, multiOutputPath);
 
     } catch (InvalidPathException e) {
       throw new IllegalArgumentException(e.getMessage());
     }
   }
 
-  private static void computeTangents(Path inputPath, Path outputPath) {
+  private static void computeTangents(Path inputPath, Path singleOutputPath, Path multiOutputPath) {
 
-    try (Scanner input = new Scanner(inputPath); BufferedWriter output = Files.newBufferedWriter(
-        outputPath)) {
+    try (Scanner input = new Scanner(inputPath)) {
 
       List<Double> numbers = new ArrayList<>();
 
@@ -47,20 +53,32 @@ public class MultithreadComputer {
         numbers.add(Double.valueOf(input.nextLine()));
       }
 
-      long startSingle = System.currentTimeMillis();
-      List<Double> singleResult = computeTanSingleThread(numbers);
-      long endSingle = System.currentTimeMillis();
-      System.out.printf("Time spent on calculation with single thread: %d%n",
-          endSingle - startSingle);
+      Instant startSingle = Instant.now();
+      List<Double> resultSingle = computeTanSingleThread(numbers);
+      Instant endSingle = Instant.now();
+      writeResultInFile(singleOutputPath, resultSingle);
 
-      long startMulti = System.currentTimeMillis();
-      List<Double> multiResult = computeTanMultiThread(numbers, 8);
-      long endMulti = System.currentTimeMillis();
-      System.out.printf("Time spent on calculation with multiple threads: %d%n",
-          endMulti - startMulti);
+      System.out.printf("Time spent on calculation with single thread: %d ms%n",
+          Duration.between(startSingle, endSingle).toMillis());
 
-      output.write("Processed " + singleResult.size() + " numbers.\n");
-      for (Double x : singleResult) {
+      Instant startMulti = Instant.now();
+      List<Double> resultMulti = computeTanMultiThread(numbers, 6);
+      Instant endMulti = Instant.now();
+      writeResultInFile(multiOutputPath, resultMulti);
+
+      System.out.printf("Time spent on calculation with multiple threads: %d ms%n",
+          Duration.between(startMulti, endMulti).toMillis());
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void writeResultInFile(Path singleOutputPath, List<Double> result) {
+    try (BufferedWriter output = Files.newBufferedWriter(singleOutputPath)) {
+      output.write("Processed " + result.size() + " numbers.\n");
+
+      for (Double x : result) {
         output.write(x + "\n");
       }
 
@@ -71,41 +89,37 @@ public class MultithreadComputer {
 
   public static List<Double> computeTanSingleThread(List<Double> numbers) {
 
-    return numbers.stream().map(Math::tan).toList();
+    List<Double> result = new ArrayList<>();
+
+    for (Double x : numbers) {
+      result.add(Math.tan(x));
+    }
+
+    return result;
   }
 
   public static List<Double> computeTanMultiThread(List<Double> numbers, int threadCount) {
 
     ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
 
-    List<TanComputer> tasks = new ArrayList<>();
-    for (Double value : numbers) {
-      tasks.add(new TanComputer(value));
-    }
-
     try {
-      List<Future<Double>> futures = executorService.invokeAll(tasks);
+      Stream<Future<Double>> futures = executorService.invokeAll(
+          numbers.stream().map(TanComputer::new).toList()).stream();
 
-      List<Double> results = new ArrayList<>();
-      for (Future<Double> x : futures) {
-        results.add(x.get(1, TimeUnit.MILLISECONDS));
-      }
+      return futures.map(doubleFuture -> {
+        try {
+          return doubleFuture.get();
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      }).toList();
 
-      return results;
-
-    } catch (InterruptedException e) {
-      throw new RuntimeException("One of the threads was interrupted.", e);
-
-    } catch (ExecutionException e) {
-      throw new RuntimeException("Exception occured in one of the threads", e);
-
-    } catch (TimeoutException e) {
+    } catch (Exception e) {
       throw new RuntimeException(e);
 
     } finally {
       executorService.shutdownNow();
     }
-
   }
 
   private static class TanComputer implements Callable<Double> {
--- src/test/java/ru/spbu/apcyb/svp/tasks/task4/Task4Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/task4/Task4Test.java
@@ -12,19 +12,20 @@ public class Task4Test {
 
     System.out.println("Calculation of 1 tan(x):");
     String[] args1 = new String[]{"src/test/resources/task4/numbers_1.txt",
-        "src/test/resources/task4/results_1.txt"};
+        "src/test/resources/task4/results_1_S.txt", "src/test/resources/task4/results_1_M.txt"};
 
     MultithreadComputer.main(args1);
 
     System.out.println("Calculation of 100 tan(x):");
     String[] args2 = new String[]{"src/test/resources/task4/numbers_100.txt",
-        "src/test/resources/task4/results_100.txt"};
+        "src/test/resources/task4/results_100_S.txt", "src/test/resources/task4/results_100_M.txt"};
 
     MultithreadComputer.main(args2);
 
     System.out.println("Calculation of 1000000 tan(x):");
     String[] args3 = new String[]{"src/test/resources/task4/numbers_1000000.txt",
-        "src/test/resources/task4/results_1000000.txt"};
+        "src/test/resources/task4/results_1000000_S.txt",
+        "src/test/resources/task4/results_1000000_M.txt"};
 
     MultithreadComputer.main(args3);
     assertTrue(true);
