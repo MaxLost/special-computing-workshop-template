230fe6d156d6aa82b33240901f3736b9c6377d8f
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-11-14 02:11:08 +0300

    Deleted perfomance comparison from regular calculating of tangents

--- src/test/java/ru/spbu/apcyb/svp/tasks/task4/Task4Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/task4/Task4Test.java
@@ -1,8 +1,11 @@
 package ru.spbu.apcyb.svp.tasks.task4;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 
 public class Task4Test {
@@ -11,24 +14,44 @@ public class Task4Test {
   void perfomanceTest() {
 
     System.out.println("Calculation of 1 tan(x):");
-    String[] args1 = new String[]{"src/test/resources/task4/numbers_1.txt",
-        "src/test/resources/task4/results_1_S.txt", "src/test/resources/task4/results_1_M.txt"};
+    String oneInput = "src/test/resources/task4/numbers_1.txt";
+    String oneSingleOut = "src/test/resources/task4/results_1_S.txt";
+    String oneMultiOut = "src/test/resources/task4/results_1_M.txt";
 
-    MultithreadComputer.main(args1);
+    MultiThreadComputer.computeTangentsWithPerfomanceLog(oneInput, oneSingleOut, oneMultiOut);
 
     System.out.println("Calculation of 100 tan(x):");
-    String[] args2 = new String[]{"src/test/resources/task4/numbers_100.txt",
-        "src/test/resources/task4/results_100_S.txt", "src/test/resources/task4/results_100_M.txt"};
+    String hundredInput = "src/test/resources/task4/numbers_100.txt";
+    String hundredSingleOut = "src/test/resources/task4/results_100_S.txt";
+    String hundredMultiOut = "src/test/resources/task4/results_100_M.txt";
 
-    MultithreadComputer.main(args2);
+    MultiThreadComputer.computeTangentsWithPerfomanceLog(hundredInput, hundredSingleOut,
+        hundredMultiOut);
 
     System.out.println("Calculation of 1000000 tan(x):");
-    String[] args3 = new String[]{"src/test/resources/task4/numbers_1000000.txt",
-        "src/test/resources/task4/results_1000000_S.txt",
-        "src/test/resources/task4/results_1000000_M.txt"};
+    String mullionInput = "src/test/resources/task4/numbers_1000000.txt";
+    String mullionSingleOut = "src/test/resources/task4/results_1000000_S.txt";
+    String mullionMultiOut = "src/test/resources/task4/results_1000000_M.txt";
 
-    MultithreadComputer.main(args3);
+    MultiThreadComputer.computeTangentsWithPerfomanceLog(mullionInput, mullionSingleOut,
+        mullionMultiOut);
     assertTrue(true);
+  }
+
+  @Test
+  void multiThreadMultiplicationTest() throws IOException {
+
+    System.out.println("Calculation of 100 tan(x):");
+    String hundredInput = "src/test/resources/task4/numbers_100.txt";
+    String hundredSingleOut = "src/test/resources/task4/results_100_S.txt";
+    String hundredMultiOut = "src/test/resources/task4/results_100_M.txt";
+
+    MultiThreadComputer.computeTangentsWithPerfomanceLog(hundredInput, hundredSingleOut,
+        hundredMultiOut);
+
+    Path singleThreadResult = Path.of(hundredSingleOut);
+    Path multiThreadResult = Path.of(hundredMultiOut);
+    assertEquals(-1, Files.mismatch(singleThreadResult, multiThreadResult));
   }
 
 }
